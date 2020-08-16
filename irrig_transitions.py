07ea14e30f259ca14a147ac52c27f56bfc2809f1
Edouard Davin <edouard.davin@env.ethz.ch>
2020-08-16 20:26:07 +0200

    correct wrong calculation of frac_irr, fix time axis

--- irrig_transitions.py
+++ irrig_transitions.py
@@ -34,20 +34,26 @@ else:
     print("error, version not found")
     
 states_luh2 = "/net/exo/landclim/data/dataset/LUH2/"+vluh+"/0.25deg_lat-lon_1y/original/states.nc"
-transi_luh2 = "/net/exo/landclim/data/dataset/LUH2/"+vluh+"/0.25deg_lat-lon_1y/original/transitions.nc"
 manage_luh2 = "/net/exo/landclim/data/dataset/LUH2/"+vluh+"/0.25deg_lat-lon_1y/original/management.nc"
 
-#calculate total irrigated fraction as a fraction of the grid cell
-frac_irr = (xr.open_dataset(states_luh2,decode_times=False).c3ann * xr.open_dataset(manage_luh2,decode_times=False).irrig_c3ann) + (xr.open_dataset(states_luh2,decode_times=False).c4ann * xr.open_dataset(manage_luh2,decode_times=False).irrig_c4ann) (xr.open_dataset(states_luh2,decode_times=False).c3per * xr.open_dataset(manage_luh2,decode_times=False).irrig_c3per) (xr.open_dataset(states_luh2,decode_times=False).c4per * xr.open_dataset(manage_luh2,decode_times=False).irrig_c4per) (xr.open_dataset(states_luh2,decode_times=False).c3nfx * xr.open_dataset(manage_luh2,decode_times=False).irrig_c3nfx) 
+blue = "/net/ch4/landclim/edavin/LUMIP/BLUE/PFT11corr/DBF2CRO_time_reg_gracorr.nc" #only for time axis
+potveg = "/net/ch4/landclim/edavin/LUMIP/BLUE/potveg_tropforestoutside40-corr_ratmax.nc"
 
+#calculate total irrigated fraction as a fraction of the grid cell
+frac_irr = (xr.open_dataset(states_luh2,decode_times=False).c3ann * xr.open_dataset(manage_luh2,decode_times=False).irrig_c3ann) + (xr.open_dataset(states_luh2,decode_times=False).c4ann * xr.open_dataset(manage_luh2,decode_times=False).irrig_c4ann) + (xr.open_dataset(states_luh2,decode_times=False).c3per * xr.open_dataset(manage_luh2,decode_times=False).irrig_c3per) + (xr.open_dataset(states_luh2,decode_times=False).c4per * xr.open_dataset(manage_luh2,decode_times=False).irrig_c4per) + (xr.open_dataset(states_luh2,decode_times=False).c3nfx * xr.open_dataset(manage_luh2,decode_times=False).irrig_c3nfx)
+
+#frac_irr = frac_irr * potveg
 
 #then calculate the transitions by taking the time derivative
 #this will result in a transition matrix with one time step less than the matrix of states (i.e. only until 2014)
-#therefore the time coordinate of the transition matrix is assigned
 
-time_transi = xr.open_dataset(transi_luh2,decode_times=False).time
+time_blue = xr.open_dataset(blue).time
 
-CROr2CROi = frac_irr.diff("time").assign_coords(time=time_transi)
+#calculate time derivative and assign time coordinates from BLUE
+CROr2CROi = frac_irr.diff("time").assign_coords(time=time_blue)
+
+#CROr2CROi = frac_irr.diff("time",label='lower') #"lower" forces new time coordinate to start at 850 (default would be 850)
+
 
 #write the transition matrix in the output file
 path_file = out_dir+"CROr2CROi_time_"+scen+".nc"
