fa57b3bf785368a50ed9c7c35586dbc4ab209934
mikewin-climsci <55045832+mikewin-climsci@users.noreply.github.com>
2021-04-16 11:58:20 +0200

    Update ensemble_proc_3_.sh

--- ensemble_proc_3_.sh
+++ ensemble_proc_3_.sh
@@ -17,7 +17,7 @@ cdo ensmean /path_to_outputfolder/diff/* /path_to_outputfolder/ensemble_mean_res
 cdo ensmean /path_to_outputfolder/exp/std/* /path_to_outputfolder/ensemble_std_response.nc
 
 # Response as ts per emitted tC:
-# 2x additional CO2 in atmosphere before sinks (GCP 2018)
+# 2.1x additional CO2 in atmosphere before sinks (GCP 2018)
 # (572-286) doubling of CO2 after pre-Industrial
 # x2.13x10xx9 factor ppm to tC
-# cdo mulc,2*(572-286)*2.13*10**9
+# cdo mulc,2.1*(572-286)*2.13*10**9
